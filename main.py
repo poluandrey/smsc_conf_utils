2bbf92948475ee720382a94cc3ab7b64c10ad6ca
Polumestny Andrey <andrey@work-station.local>
2022-12-02 12:14:36 +0400

    add test

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import argparse
+import os
+import re
+
+from pathlib import Path
+from typing import List, Tuple, NamedTuple
+
+
+class SmppConnections(NamedTuple):
+    sme_id: int
+    type: str
+    system_id: str
+    password: str
+    system_type: str
+
+
+class SmppConfig(NamedTuple):
+    connections: List[SmppConnections]
+    rules: List[NamedTuple]
 
 
 def argument_parser():
@@ -17,40 +35,103 @@ def argument_parser():
         choices=('smpp', 'sccp_gt'),
         required=True,
     )
+    check_config_parser.add_argument(
+        '--file', '-f',
+        help='path to file',
+        required=True
+    )
+    check_config_parser.set_defaults(callback=check_config_callback)
 
     subparser.add_parser(
         'load',
         help='load provided config into DB',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument(
-        '--file', '-f',
-        help='path to file',
-        required=True
-    )
     return parser.parse_args()
 
 
-def smpp_conf_parser(conf_path):
-    pass
+def gt_conf_parser(file: Path) -> List[Tuple]:
+    parsed_conf = []
+    with open(file, 'r') as f_in:
+        for line in f_in:
+            if re.search(r'^\d', line):
+                gt_params = re.sub(' +', ' ', line).split(' ')
+                gt_id, gt, link = gt_params
+                parsed_conf.append(
+                    (gt_id.strip(), gt.strip(), link.strip()))
+    return parsed_conf
 
 
-def sccp_gt_parser(conf_path):
-    pass
+def sme_parser(line) -> SmppConnections:
+    params = re.sub(' +', ' ', line).split(' ')
+    return SmppConnections(
+        int(params[1]),
+        params[2].replace('"', ''),
+        params[3].replace('"', ''),
+        params[4].replace('"', ''),
+        params[5].replace('"', '')
+    )
+
+
+def smpp_conf_parser(file: Path) -> SmppConfig:
+    connections = []
+    rules = []
+    with open(file, 'r') as f_in:
+        for line in f_in:
+            if re.search('^sme', line):
+                connections.append(sme_parser(line.strip()))
+            elif re.search('^rule', line):
+                pass
+    return SmppConfig(connections, rules)
+
+
+def check_gt_conf(parsed_conf) -> str:
+    gt_ids = [values[0] for values in parsed_conf]
+    unique_ids = list(set(gt_ids))
+    for gt_id in unique_ids:
+        gt_ids.remove(gt_id)
+    if gt_ids:
+        return f'WARNING!\n{", ".join(gt_ids)} ids are duplicated'
+    return 'All looks good'
+
+
+def check_smpp_conf(parsed_conf):
+    sme_ids = [connection.sme_id for connection in parsed_conf.connections]
+    unique_sme_ids = list(set(sme_ids))
+    for sme_id in unique_sme_ids:
+        sme_ids.remove(sme_id)
+    if sme_ids:
+        return f'WARNING\n{", ".join(sme_ids)} sme are duplicated'
+    return 'All looks good'
+
+
+def get_file_path(file):
+    file = Path(file)
+    if os.path.isabs(file):
+        return file
+    else:
+        return os.path.abspath(file)
+
+
+def check_config_callback(arguments) -> str:
+    print(arguments)
+    file_path = get_file_path(arguments.file)
+    if arguments.config == 'smpp':
+        parsed_conf = smpp_conf_parser(file_path)
+        return check_smpp_conf(parsed_conf)
+    elif arguments.config == 'sccp_gt':
+        parsed_conf = gt_conf_parser(file_path)
+        return check_gt_conf(parsed_conf)
 
 
 def db_loader(smpp_rules, gt_lis):
     pass
 
 
-def check_conf_correct(conf_path):
-    pass
-
-
 def main():
-    cli = argument_parser()
+    args = argument_parser()
+    args.callback(args)
 
 
 if __name__ == '__main__':
-    print('start')
     main()
--- test_conf_parser.py
+++ test_conf_parser.py
@@ -1,2 +1,43 @@
+from pathlib import Path
+from main import gt_conf_parser, check_gt_conf
+
 import pytest
 
+
+@pytest.fixture
+def gt_conf_file(request):
+    conf_type = request.param
+    if 'bad' == conf_type or 'relative_path' == conf_type:
+        file = Path('conf/bad_sccp_gt_list.dat')
+    elif 'good' == conf_type:
+        file = Path('conf/good_sccp_gt_list.dat')
+    elif 'absolute_path' == conf_type:
+        file = Path('/Users/andrey/Documents/work/smsc_conf_parser/conf/bad_sccp_gt_list.dat')
+    return file
+
+
+@pytest.mark.parametrize('gt_conf_file, result',
+                         [
+                             ('bad', 6),
+                             ('good', 3),
+                             ('absolute_path', 6),
+                             ('relative_path', 6),
+                         ],
+                         indirect=['gt_conf_file']
+                         )
+def test_gt_conf_parser(gt_conf_file, result):
+    parsed_conf = gt_conf_parser(gt_conf_file)
+    assert len(parsed_conf) == result, f'Wrong count of line in parsed conf. Expected 6 got {len(parsed_conf)}'
+
+
+@pytest.mark.parametrize('gt_conf_file, result',
+                         [
+                             ('bad', 'WARNING'),
+                             ('good', 'All looks good'),
+                         ],
+                         indirect=['gt_conf_file']
+                         )
+def test_check_gt_conf(gt_conf_file, result):
+    parsed_conf = gt_conf_parser(gt_conf_file)
+    check_result = check_gt_conf(parsed_conf)
+    assert check_result.startswith(result), f'Bad config file checking. Answer did not contain {result}'
